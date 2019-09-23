5c922b775b72ce111afd4203f23a3c8f819b5a7e
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-09-23 09:06:27 +0200

    add anaytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-root-import',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-root-import",
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-143450655-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
