e3398b28e3bdd74b2d022cb86e2cba1231f025b3
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-03-10 23:35:56 +0100

    Updates

---
+++ component---src-pages-about-index-js-89d8d883d9e967725f77.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{O0dG:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Bl7J"),r=a("vrFN"),c=a("BEI2"),u=a.n(c),i=a("Wrlk");t.default=function(){return l.a.createElement(o.a,{location:"/about"},l.a.createElement(r.a,{title:"About"}),l.a.createElement("div",{className:u.a.root},l.a.createElement(i.a,null)))}}}]);
+//# sourceMappingURL=component---src-pages-about-index-js-89d8d883d9e967725f77.js.map
