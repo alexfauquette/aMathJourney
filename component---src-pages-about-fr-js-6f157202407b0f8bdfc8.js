ea45846b635d2ac7092b1bfcc905bf2920e7c76b
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-03-11 00:45:17 +0100

    Updates

--- component---src-pages-about-fr-js-6f157202407b0f8bdfc8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3do2":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),r=t.n(n),o=t("Bl7J"),c=t("vrFN"),l=t("BEI2"),s=t.n(l),u=t("syPx");a.default=function(){return r.a.createElement(o.a,{location:"/about",currentLanguage:"Français"},r.a.createElement(c.a,{title:"A propos"}),r.a.createElement("div",{className:s.a.root},r.a.createElement(u.a,null)))}}}]);
-//# sourceMappingURL=component---src-pages-about-fr-js-6f157202407b0f8bdfc8.js.map
