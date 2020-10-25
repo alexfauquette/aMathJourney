615f48e4585d94d6e6f64c83c1407e7c0827b19b
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-10-25 23:11:05 +0100

    allow to highligh a trajectory

--- src/components/linearSystem/SystemPlot.js
+++ src/components/linearSystem/SystemPlot.js
@@ -1,26 +1,37 @@
 import { element } from "prop-types"
 import React, { useState, useEffect } from "react"
 
-const computeTrajectory = (x0, lambda1, lambda2, s1, s2) => {
+const computeTrajectory = (x0, lambda1, lambda2, s1, s2, t0, t1) => {
   const dt = 0.05
   const rep = []
 
   if (lambda1.y === 0) {
-    for (let t = -10; t < 10; t += dt) {
+    const det = s1.x * s2.y - s1.y * s2.x
+
+    const z0 = {
+      //S^-1 x
+      x: (s2.y * x0.x - s2.x * x0.y) / det,
+      y: (-s1.y * x0.x + s1.x * x0.y) / det,
+    }
+    for (let t = t0; t < t1; t += dt) {
       rep.push({
         x:
-          Math.exp(t * lambda1.x) * x0.x * s1.x +
-          Math.exp(t * lambda2.x) * x0.y * s2.x,
+          Math.exp(t * lambda1.x) * z0.x * s1.x +
+          Math.exp(t * lambda2.x) * z0.y * s2.x,
         y:
-          Math.exp(t * lambda1.x) * x0.x * s1.y +
-          Math.exp(t * lambda2.x) * x0.y * s2.y,
+          Math.exp(t * lambda1.x) * z0.x * s1.y +
+          Math.exp(t * lambda2.x) * z0.y * s2.y,
       })
     }
+    return rep //.map(({ x, y }) => ({
+    //   x: s1.x * x + s2.x * y,
+    //   y: s1.y * x + s2.y * y,
+    // }))
   } else {
     const r0 = Math.sqrt(x0.x ** 2 + x0.y ** 2)
-    const theta0 = Math.atan2(x0.x, x0.y)
+    const theta0 = Math.atan2(x0.y, x0.x)
 
-    for (let t = -5; t < 5; t += dt) {
+    for (let t = t0; t < t1; t += dt) {
       rep.push({
         x:
           Math.cos(theta0 - Math.abs(lambda1.y) * t) *
@@ -41,74 +52,95 @@ const computeLines = (lambda1, lambda2, s1, s2) => {
 
   if (lambda1.y != 0) {
     for (let x = -5; x < 6; x += 2) {
-      rep.push(computeTrajectory({ x, y: 0 }, lambda1, lambda2, s1, s2))
-      rep.push(computeTrajectory({ x: 0, y: x - 1 }, lambda1, lambda2, s1, s2))
+      rep.push(computeTrajectory({ x, y: 0 }, lambda1, lambda2, s1, s2, -10, 5))
+      rep.push(
+        computeTrajectory({ x: 0, y: x - 1 }, lambda1, lambda2, s1, s2, -10, 5)
+      )
     }
   } else {
     for (let x = -5; x < 6; x++) {
-      rep.push(computeTrajectory({ x, y: 5 }, lambda1, lambda2, s1, s2))
-      rep.push(computeTrajectory({ x, y: -5 }, lambda1, lambda2, s1, s2))
+      rep.push(computeTrajectory({ x, y: 5 }, lambda1, lambda2, s1, s2, -5, 5))
+      rep.push(computeTrajectory({ x, y: -5 }, lambda1, lambda2, s1, s2, -5, 5))
     }
     for (let y = -4; y < 5; y++) {
-      rep.push(computeTrajectory({ x: 5, y }, lambda1, lambda2, s1, s2))
-      rep.push(computeTrajectory({ x: -5, y }, lambda1, lambda2, s1, s2))
+      rep.push(computeTrajectory({ x: 5, y }, lambda1, lambda2, s1, s2, -5, 5))
+      rep.push(computeTrajectory({ x: -5, y }, lambda1, lambda2, s1, s2, -5, 5))
     }
   }
   return rep
 }
 
+const ELEMENT_SIZE = 500
+const VIEW_SIZE = 100
+const MAX_X = 5
 const SystemPlot = ({ lambda1, lambda2 }) => {
-  const data = computeLines(lambda1, lambda2, { x: 1, y: 1 }, { x: 4, y: 1 })
+  const s1 = { x: 1, y: 1 }
+  const s2 = { x: 4, y: 1 }
+  const data = computeLines(lambda1, lambda2, s1, s2)
 
-  console.log(data[0])
+  const [pointer, setPointer] = useState(null)
+  const [path, setPath] = useState([])
+
+  useEffect(() => {
+    if (pointer === null) {
+      setPath(null)
+    } else {
+      setPath(computeTrajectory(pointer, lambda1, lambda2, s1, s2, 0, 10))
+    }
+  }, [pointer])
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="-50 -50 100 100"
-      style={{ width: "500px", height: "500px" }}
-      // onMouseUp={() => setDraggId(null)}
-      // onMouseLeave={() => setDraggId(null)}
-      // onMouseMove={
-      //   draggId === null
-      //     ? null
-      //     : (event) => {
-      //         let x = (5 * (event.nativeEvent.offsetX - 250)) / 250
-      //         let y = (5 * (event.nativeEvent.offsetY - 250)) / 250
-      //         x = x - (x % 0.1)
-      //         y = y - (y % 0.1)
-
-      //         if (lambda2.y === 0 && Math.abs(y) < 0.75) {
-      //           // we are on real axis and do not move to much from it so we stay
-      //           if (draggId === 1) {
-      //             setLambda1({ ...lambda1, x })
-      //           } else {
-      //             setLambda2({ ...lambda2, x })
-      //           }
-      //         } else {
-      //           // ware (or are going) to be in conjate complex case
-      //           if (draggId === 1) {
-      //             setLambda2({ x, y: -y })
-      //             setLambda1({ x, y })
-      //           } else {
-      //             setLambda1({ x, y: -y })
-      //             setLambda2({ x, y })
-      //           }
-      //         }
-      //       }
-      // }
+      viewBox={`-${VIEW_SIZE / 2} -${VIEW_SIZE / 2} ${VIEW_SIZE} ${VIEW_SIZE}`}
+      style={{ width: `${ELEMENT_SIZE}px`, height: `${ELEMENT_SIZE}px` }}
+      onMouseLeave={() => setPointer(null)}
+      onMouseMove={(event) => {
+        const x =
+          (MAX_X * (event.nativeEvent.offsetX - ELEMENT_SIZE / 2)) /
+          ELEMENT_SIZE
+        const y =
+          (MAX_X * (event.nativeEvent.offsetY - ELEMENT_SIZE / 2)) /
+          ELEMENT_SIZE
+        setPointer({
+          x: x,
+          y: y,
+        })
+      }}
     >
       <path d={`M -50 0 L 50 0`} style={{ stroke: "black" }} />
       <path d={`M 0 -50 L 0 50`} style={{ stroke: "black" }} />
-
       {data.map((element, index) => (
         <path
           key={index}
           d={`M ${element
-            .map(({ x, y }) => `${10 * x} ${10 * y}`)
+            .map(
+              ({ x, y }) =>
+                `${(VIEW_SIZE / MAX_X) * x} ${(VIEW_SIZE / MAX_X) * y}`
+            )
             .join(" L ")}`}
           style={{ stroke: "green", fill: "none" }}
         />
       ))}
+      {pointer && pointer.x && pointer.y && (
+        <circle
+          cx={(VIEW_SIZE / MAX_X) * pointer.x}
+          cy={(VIEW_SIZE / MAX_X) * pointer.y}
+          r={2}
+          style={{ fill: "red" }}
+        />
+      )}
+      {path && (
+        <path
+          d={`M ${path
+            .map(
+              ({ x, y }) =>
+                `${(VIEW_SIZE / MAX_X) * x} ${(VIEW_SIZE / MAX_X) * y}`
+            )
+            .join(" L ")}`}
+          style={{ stroke: "red", fill: "none" }}
+        />
+      )}
     </svg>
   )
 }
--- src/components/linearSystem/eigenValueSelector.js
+++ src/components/linearSystem/eigenValueSelector.js
@@ -1,5 +1,4 @@
-import { element } from "prop-types"
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Slider from "@material-ui/core/Slider"
 import SystemPlot from "./SystemPlot"
 
